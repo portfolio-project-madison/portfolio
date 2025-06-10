cc9c9d0f8bbc93e00158e6be949615acf4a15b19
madisonnnn <madisontolentino@gmail.com>
2025-06-10 16:05:03 -0400

    deployment

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS, cross_origin
-from app.routes.inforoutes import info_bp
+from server.app.routes.inforoutes import info_bp
 
 def create_app():
     app = Flask(__name__,  static_folder='static', static_url_path='')
